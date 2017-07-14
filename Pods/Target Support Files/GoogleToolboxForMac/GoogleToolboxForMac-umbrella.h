387513562ab3749f54e459455528b69aea78374f
Alec Gorge <alecgorge@gmail.com>
2017-07-14 17:17:45 +0200

    update pods

--- Pods/Target Support Files/GoogleToolboxForMac/GoogleToolboxForMac-umbrella.h
+++ Pods/Target Support Files/GoogleToolboxForMac/GoogleToolboxForMac-umbrella.h
@@ -10,9 +10,14 @@
 #endif
 #endif
 
+#import "GTMDebugSelectorValidation.h"
+#import "GTMDebugThreadValidation.h"
+#import "GTMMethodCheck.h"
 #import "GTMDefines.h"
 #import "GTMLogger.h"
 #import "GTMNSData+zlib.h"
+#import "GTMNSDictionary+URLArguments.h"
+#import "GTMNSString+URLArguments.h"
 
 FOUNDATION_EXPORT double GoogleToolboxForMacVersionNumber;
 FOUNDATION_EXPORT const unsigned char GoogleToolboxForMacVersionString[];

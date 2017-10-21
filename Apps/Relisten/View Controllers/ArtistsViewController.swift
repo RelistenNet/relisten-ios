35be31130fd735f69ca8ca05e2ce30fcad17f46e
Alec Gorge <alecgorge@gmail.com>
2017-10-21 18:47:45 -0400

    Furthur progress
    Former-commit-id: 135cba44966edca70953f19b8922809c9cb67e2a

--- Apps/Relisten/View Controllers/ArtistsViewController.swift
+++ Apps/Relisten/View Controllers/ArtistsViewController.swift
@@ -13,19 +13,35 @@ import LayoutKit
 
 class ArtistsViewController: RelistenTableViewController<[ArtistWithCounts]> {
 
+    var artistIdChangedEventHandler: Disposable?
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
         title = "Relisten"
+        
+        artistIdChangedEventHandler = MyLibraryManager.sharedInstance.favoriteArtistIdsChanged.addHandler(target: self, handler: ArtistsViewController.favoritesChanged)
+    }
+    
+    deinit {
+        if let e = self.artistIdChangedEventHandler {
+            e.dispose()
+        }
+    }
+    
+    func favoritesChanged(ids: Set<Int>) {
+        render()
     }
 
     override var resource: Resource? { get { return api.artists() } }
     
     override func render(forData: [ArtistWithCounts]) {
         layout {
-            let toLayout : (ArtistWithCounts) -> ArtistLayout = { ArtistLayout(artist: $0) }
+            let favArtistIds = MyLibraryManager.sharedInstance.artistIds
+            let toLayout : (ArtistWithCounts) -> ArtistLayout = { ArtistLayout(artist: $0, withFavoritedArtists: favArtistIds) }
             
             return [
+                forData.filter({ favArtistIds.contains($0.id) }).map(toLayout).asSection("Favorites"),
                 forData.filter({ $0.featured > 0 }).map(toLayout).asSection("Featured"),
                 forData.map(toLayout).asSection("All \(forData.count) Artists")
             ]
@@ -42,7 +58,20 @@ class ArtistsViewController: RelistenTableViewController<[ArtistWithCounts]> {
         tableView.deselectRow(at: indexPath, animated: true)
         
         if let d = latestData {
-            navigationController?.pushViewController(ArtistViewController(artist: d[indexPath.row]), animated: true)
+            let favArtistIds = MyLibraryManager.sharedInstance.artistIds
+            
+            var artist: ArtistWithCounts
+            if indexPath.section == 0 {
+                artist = d.filter({ favArtistIds.contains($0.id) })[indexPath.row]
+            }
+            else if indexPath.section == 1 {
+                artist = d.filter({ $0.featured > 0 })[indexPath.row]
+            }
+            else {
+                artist = d[indexPath.row]
+            }
+            
+            navigationController?.pushViewController(ArtistViewController(artist: artist), animated: true)
         }
     }
 }

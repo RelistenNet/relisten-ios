8919028614f3f0c8d154a4cbb35eb3bbc4f22198
Alec Gorge <alecgorge@gmail.com>
2018-06-02 17:58:54 -0400

    My modifications
    Former-commit-id: 730d43d3797344c735a5aa449fdfd5d852a8a595

--- Pods/Observable/Observable/Classes/Observable.swift
+++ Pods/Observable/Observable/Classes/Observable.swift
@@ -1,32 +1,53 @@
 import Foundation
 
+private var ObservableUniqueID = (0...).makeIterator()
+public let ObservableObserverQueue = DispatchQueue(label: "eventDispatchingQueue", attributes: .concurrent)
+
 public final class Observable<T> {
 
     public typealias Observer = (T, T?) -> Void
 
     private var observers: [Int: Observer] = [:]
     private var uniqueID = (0...).makeIterator()
+    
+    private let queue: DispatchQueue
 
     public var value: T {
         didSet {
-            observers.values.forEach { $0(value, oldValue) }
+            let val = value
+            let oval = oldValue
+            
+            queue.sync {
+                observers.values.forEach { cb in ObservableObserverQueue.async { cb(val, oval) } }
+            }
         }
     }
 
     public init(_ value: T) {
         self.value = value
+        
+        queue = DispatchQueue(label: "observable queue #\(ObservableUniqueID.next()!)", attributes: .concurrent)
     }
 
     public func observe(_ observer: @escaping Observer) -> Disposable {
+        return observe(includingInitial: true, observer)
+    }
+    
+    public func observe(includingInitial: Bool = true, _ observer: @escaping Observer) -> Disposable {
         guard let id = uniqueID.next() else { fatalError("There should always be a next unique id") }
-
-        observers[id] = observer
-        observer(value, nil)
-
+        
         let disposable = Disposable { [weak self] in
             self?.observers[id] = nil
         }
-
+        
+        queue.async(flags: .barrier) {
+            self.observers[id] = observer
+        }
+        
+        if includingInitial {
+            observer(self.value, nil)
+        }
+        
         return disposable
     }
 

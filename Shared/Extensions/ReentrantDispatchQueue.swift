209bc5fb4911e92b029f7fe1ac6b7a4f54f99d1e
Jacob Farkas <jacobf@rkas.net>
2018-08-03 12:27:27 -0700

    ReentrantDispatchQueue is either a great idea or a terrible idea and I'm not sure which it is

---
+++ Shared/Extensions/ReentrantDispatchQueue.swift
@@ -0,0 +1,61 @@
+//
+//  DispatchQueue.swift
+//  RelistenShared
+//
+//  Created by Jacob Farkas on 7/27/18.
+//  Copyright © 2018 Alec Gorge. All rights reserved.
+//
+
+import Foundation
+
+// This class wraps a DispatchQueue and allows for sync calls to itself without deadlocking.
+// Note: blocks that are async'ed while currently on the queue will run immediately rather than enqueuing
+public class ReentrantDispatchQueue {
+    static public let main : ReentrantDispatchQueue = ReentrantDispatchQueue.init(withMainQueue: true)
+    
+    private let queueKey = DispatchSpecificKey<Int>()
+    private let queue : DispatchQueue
+    
+    public init(queue: DispatchQueue) {
+        self.queue = queue
+        self.queue.setSpecific(key: queueKey, value: 1)
+    }
+    
+    public convenience init(_ label: String) {
+        let q = DispatchQueue(label: label)
+        self.init(queue: q)
+    }
+    
+    public convenience init(withMainQueue: Bool) {
+        if withMainQueue {
+            self.init(queue: DispatchQueue.main)
+        } else {
+            self.init("net.relisten.reentrantQueue")
+        }
+    }
+    
+    public convenience init(label: String, qos: DispatchQoS = .default, attributes: DispatchQueue.Attributes = DispatchQueue.Attributes(), autoreleaseFrequency: DispatchQueue.AutoreleaseFrequency = .inherit, target: DispatchQueue? = nil) {
+        let q = DispatchQueue(label: label, qos: qos, attributes: attributes, autoreleaseFrequency: autoreleaseFrequency, target: target)
+        self.init(queue: q)
+    }
+
+    public func sync(execute block: () -> Swift.Void) {
+        if DispatchQueue.getSpecific(key: queueKey) != nil {
+            block()
+        } else {
+            self.queue.sync(execute: block)
+        }
+    }
+    
+    public func async(_ block: @escaping () -> Swift.Void) {
+        if DispatchQueue.getSpecific(key: queueKey) != nil {
+            block()
+        } else {
+            self.queue.async(execute: block)
+        }
+    }
+}
+
+public func performOnMainQueueSync(_ block: () -> Swift.Void) {
+    ReentrantDispatchQueue.main.sync(execute: block)
+}

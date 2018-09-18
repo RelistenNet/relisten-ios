bf367fdd027554d9b79c87e652f9f3b16188f3cd
Jacob Farkas <jacobf@rkas.net>
2018-09-17 22:17:24 -0700

    Add a comment to the feedback email to encourage more feedback

--- Shared/Helpers/UserFeedback.swift
+++ Shared/Helpers/UserFeedback.swift
@@ -23,7 +23,7 @@ public class UserFeedback  {
         let versionString = "Relisten \(RelistenApp.sharedApp.appVersion) (\(RelistenApp.sharedApp.appBuildVersion))"
         var feedbackConfig = FeedbackConfiguration(recipients: ["ios@relisten.net"])
         feedbackConfig.title = "[Bug Report: \(versionString)]"
-        feedbackConfig.body = "\n\n\nConfiguration Information (Please don't delete):\n\t\(versionString)\n\tCrash log identifier \(RelistenApp.sharedApp.crashlyticsUserIdentifier)"
+        feedbackConfig.body = "\n\n\n[Please write a brief description of the problem you're seeing above this line]\n\n\nConfiguration Information (Please don't delete):\n\t\(versionString)\n\tCrash log identifier \(RelistenApp.sharedApp.crashlyticsUserIdentifier)"
         let config = Configuration(logCollector: RelistenLogCollector(), feedbackConfiguration: feedbackConfig)
         pinpointKit = PinpointKit(configuration: config)
     }

f9338c94dcba39a2183b117a533f978a90560f53
Jacob Farkas <jacobf@rkas.net>
2018-10-05 15:45:02 -0700

    Update the song and venue view controllers

--- Shared/View Controllers/Songs/SongsViewController.swift
+++ Shared/View Controllers/Songs/SongsViewController.swift
@@ -11,12 +11,14 @@ import Foundation
 import UIKit
 
 import Siesta
-import LayoutKit
+import AsyncDisplayKit
 import SINQ
 
-class SongsViewController: RelistenTableViewController<[SongWithShowCount]> {
+// TODO: Combine this with VenuesViewController into something more abstract. The code is practically identical
+class SongsViewController: RelistenAsyncTableViewController<[SongWithShowCount]> {
     
     let artist: ArtistWithCounts
+    var songs: [Grouping<String, SongWithShowCount>] = []
     
     public required init(artist: ArtistWithCounts) {
         self.artist = artist
@@ -24,12 +26,12 @@ class SongsViewController: RelistenTableViewController<[SongWithShowCount]> {
         super.init(useCache: true, refreshOnAppear: true)
     }
     
-    public required init(useCache: Bool, refreshOnAppear: Bool, style: UITableView.Style = .plain) {
-        fatalError("init(useCache:refreshOnAppear:) has not been implemented")
-    }
-    
     required init?(coder aDecoder: NSCoder) {
         fatalError()
+    }
+    
+    public required init(useCache: Bool, refreshOnAppear: Bool, style: UITableView.Style = .plain) {
+        fatalError("init(useCache:refreshOnAppear:) has not been implemented")
     }
     
     override func viewDidLoad() {
@@ -38,57 +40,61 @@ class SongsViewController: RelistenTableViewController<[SongWithShowCount]> {
         title = "Songs"
     }
     
-    override func has(oldData: [SongWithShowCount], changed: [SongWithShowCount]) -> Bool {
-        return oldData.count != changed.count
-    }
-    
     override var resource: Resource? { get { return api.songs(byArtist: artist) } }
     
-    var groups: [Grouping<String, SongWithShowCount>]? = nil
-    
-    override func render(forData: [SongWithShowCount]) {
-        groups = sinq(forData)
+    public override func dataChanged(_ data: [SongWithShowCount]) {
+        songs = sinq(data)
             .groupBy({
                 return $0.sortName.groupNameForTableView()
             })
             .toArray()
             .sorted(by: { (a, b) -> Bool in
                 return a.key <= b.key
-            })
+        })
+    }
+    
+    func songForIndexPath(_ indexPath: IndexPath) -> SongWithShowCount? {
+        var retval : SongWithShowCount? = nil
+        if indexPath.section >= 0, indexPath.section < songs.count {
+            let allSongs = songs[indexPath.section].values
+            if indexPath.row >= 0, indexPath.row < allSongs.count() {
+                retval = allSongs.elementAt(indexPath.row)
+            }
+        }
+        return retval
+    }
+    
+    //MARK: Table Data Source
+    
+    func numberOfSections(in tableNode: ASTableNode) -> Int {
+        return songs.count
+    }
+    
+    func tableNode(_ tableNode: ASTableNode, numberOfRowsInSection section: Int) -> Int {
+        return songs[section].values.count()
+    }
+    
+    func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
+        guard section != 0 else {
+            return nil
+        }
         
-        layout {
-            guard let g = self.groups else {
-                return [Section(header: nil, items: [], footer: nil)]
-            }
-            
-            return g.map {
-                LayoutsAsSingleSection(items: $0.values.map({ (v) in
-                    return SongLayout(song: v)
-                }), title: $0.key as String?)
-            }
+        return songs[section].key
+    }
+    
+    func tableNode(_ tableNode: ASTableNode, nodeBlockForRowAt indexPath: IndexPath) -> ASCellNodeBlock {
+        if let song = songForIndexPath(indexPath) {
+            return { SongNode(song: song) }
+        } else {
+            return { ASCellNode() }
         }
     }
     
-    override func sectionIndexTitles(for tableView: UITableView) -> [String]? {
-        if let g = groups {
-            return g.map({ $0.key })
-        }
+    func tableNode(_ tableNode: ASTableNode, didSelectRowAt indexPath: IndexPath) {
+        tableNode.deselectRow(at: indexPath, animated: true)
         
-        return nil
-    }
-    
-    override func tableView(_ tableView: UITableView, cell: UITableViewCell, forRowAt indexPath: IndexPath) -> UITableViewCell {
-        cell.accessoryType = .disclosureIndicator
-        
-        return cell
-    }
-    
-    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
-        tableView.deselectRow(at: indexPath, animated: true)
-        
-        if let g = groups {
-            let s = g[indexPath.section].values.elementAt(indexPath.row)
-            navigationController?.pushViewController(SongViewController(artist: artist, song: s), animated: true)
+        if let song = songForIndexPath(indexPath) {
+            navigationController?.pushViewController(SongViewController(artist: artist, song: song), animated: true)
         }
     }
 }

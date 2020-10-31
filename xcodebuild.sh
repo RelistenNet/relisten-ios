98d20e5ec428a0106ca9e2f477ca7db9ff08139c
Jacob Farkas <jacobf@rkas.net>
2020-10-31 13:11:54 -0700

    Add a test phase to the github CI action

--- xcodebuild.sh
+++ xcodebuild.sh
@@ -17,6 +17,10 @@ DESTINATION=`xcrun simctl list devices available iPhone | sed -nE 's/^ +(iPhone[
 XCODE_ARGS="COMPILER_INDEX_STORE_ENABLE=NO CODE_SIGNING_REQUIRED=NO"
 XCODE_DESTINATION="platform=$PLATFORM,name=$DESTINATION"
 
+BUILD_COMMAND="build-for-testing"
+if [[ $1 == "test" ]]; then
+    BUILD_COMMAND="test-without-building"
+fi
 
 # bundle exec xcpretty -v
-set -o pipefail && xcodebuild clean build -sdk iphonesimulator -workspace Relisten.xcworkspace -scheme Relisten -destination="$XCODE_DESTINATION" $XCODE_ARGS | bundle exec xcpretty -f `xcpretty-travis-formatter`
+set -o pipefail && xcodebuild $BUILD_COMMAND -sdk iphonesimulator -workspace Relisten.xcworkspace -scheme Relisten -destination "$XCODE_DESTINATION" $XCODE_ARGS | bundle exec xcpretty -f `xcpretty-travis-formatter`
